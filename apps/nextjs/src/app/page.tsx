a8f9cf831aa6a611f88621316d195fdda99e88e1
x-sss-x <manu48617@gmail.com>
2025-07-02 10:00:04 +0530

    Update pnpm-lock and package.json to include @splinetool/react-spline and adjust styles in globals.css for improved theming. Enhance homepage layout with new branding and Spline integration.

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import Spline from "@splinetool/react-spline/next";
+
+import { Button } from "@nxss/ui/button";
+
 import { HydrateClient, prefetch, trpc } from "~/trpc/server";
 
 export default function HomePage() {
@@ -5,10 +10,33 @@ export default function HomePage() {
 
   return (
     <HydrateClient>
-      <main className="container h-screen py-16">
-        <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Nexuss ERP
-        </h1>
+      <main className="container flex h-screen items-center justify-between py-16">
+        <div className="space-y-4">
+          <div className="mb-14 flex items-center gap-3">
+            <Image
+              src={"/nexuss-logo.png"}
+              height={64}
+              width={64}
+              alt="Nexuss Logo"
+              className="dark:invert"
+            />
+            <h3 className="font-mono text-4xl font-bold">NexussERP</h3>
+          </div>
+          <h1 className="text-5xl font-extrabold leading-tight tracking-tight">
+            Everything Your Institution Needs. Nothing It Doesn't.
+          </h1>
+          <p className="text-xl text-muted-foreground">
+            Nexus ERP trims the clutter and delivers only what
+            matters—beautifully and intuitively.
+          </p>
+          <div className="space-x-2.5">
+            <Button size={"lg"}>Get Started</Button>
+            <Button variant={"secondary"} size={"lg"}>
+              Sign in
+            </Button>
+          </div>
+        </div>
+        <Spline scene="https://prod.spline.design/EvWngk-QnczF24xr/scene.splinecode" />
       </main>
     </HydrateClient>
   );
