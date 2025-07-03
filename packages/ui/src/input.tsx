85284c685d0734b8621201f7a75df82ba71229e3
x-sss-x <manu48617@gmail.com>
2025-07-03 14:02:33 +0530

    Update CSS variables for link colors and refactor Input component to use forwardRef for better integration with React. Adjust button styles for improved visual consistency.

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,15 +1,22 @@
-import type { ComponentProps } from "react";
+import * as React from "react";
 
 import { cn } from "@nxss/ui";
 
-export function Input({ className, ...props }: ComponentProps<"input">) {
-  return (
-    <input
-      className={cn(
-        "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-input bg-input/25 px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-input/25 file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
